7d90993b0b6333fbcf21d9ca90c68664021ac1a1
shivanigod <sgodse@uw.edu>
2024-07-08 16:38:20 -0700

    Update classes.py

--- Intermediate OOP Framework Code/classes.py
+++ Intermediate OOP Framework Code/classes.py
@@ -1,10 +1,10 @@
-# Python Intermediate Object Oriented Programming #3 - Class Variables
+# Python Intermediate Object Oriented Programming #3 - Class Methods and Static Methods
 
 
 
 class House: 
 
-	roof = False
+	roof = True
 	
 	#the init() method is called as soon as the instance is created- meaning that we don't have to call it- python will call it for us
 	def __init__(self, doors, windows, floors): 
@@ -12,19 +12,19 @@ class House:
 		self.windows = windows
 		self.floors = floors
 
-	def clean_roof(self): 
-		if House.roof: 
-			print('Cleaning roof')
-		else: 
-			print('No roof to be cleaned')
+	@classmethod #turning this method into a class method
+	def myMethod(cls, roof):
+		cls.roof = roof
+
+#doesn't deal with the instance of the class OR with the class itself
+	@staticmethod
+	def leave_house(time_of_day): 
+		print(f'Leaving house at {time_of_day}')
 
 house1 = House(2, 6, 3)
-print(house1.doors, house1.windows, house1.floors)
-house1.clean_roof
+house2 = House(2, 6, 3)
 
+House.myMethod(False)
 print(House.roof)
-house1.roof = True #overrides the classes state 
-print(house1.roof)
-
-print(house1.__dict__) #prints out all the instance attributes of house1 (doors, windows, floors)
-house1.clean_roof()
+House.leave_house('2 PM')
+house1.leave_house('3 PM')
