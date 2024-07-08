b2b9390596b9ca1638e091c639b354654153e19a
shivanigod <sgodse@uw.edu>
2024-07-08 16:31:02 -0700

    Updated

--- Intermediate OOP Framework Code/classes.py
+++ Intermediate OOP Framework Code/classes.py
@@ -1,8 +1,10 @@
-# Python Intermediate Object Oriented Programming #2 - Attributes
-# Think of classes as an instance/blueprint for an actual object that will be created using the class
+# Python Intermediate Object Oriented Programming #3 - Class Variables
+
 
 
 class House: 
+
+	roof = False
 	
 	#the init() method is called as soon as the instance is created- meaning that we don't have to call it- python will call it for us
 	def __init__(self, doors, windows, floors): 
@@ -10,5 +12,19 @@ class House:
 		self.windows = windows
 		self.floors = floors
 
+	def clean_roof(self): 
+		if House.roof: 
+			print('Cleaning roof')
+		else: 
+			print('No roof to be cleaned')
+
 house1 = House(2, 6, 3)
 print(house1.doors, house1.windows, house1.floors)
+house1.clean_roof
+
+print(House.roof)
+house1.roof = True #overrides the classes state 
+print(house1.roof)
+
+print(house1.__dict__) #prints out all the instance attributes of house1 (doors, windows, floors)
+house1.clean_roof()
