65f8d2d4c7edaa06c561511df2b804f992e4c22f
Maya Bhat <mayasbhat1@gmail.com>
2024-07-08 18:27:23 -0700

    initial algorithm

--- main.py
+++ main.py
@@ -27,6 +27,30 @@ import requests
 
 
 ###############################################
+class Restaurant:
+    def __init__(self, name, address, price, rating, latitude, longitude, is_closed, distance):
+        self.name = name
+        self.address = address
+        self.price = price
+        self.rating = float(rating)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.is_closed = is_closed
+        self.distance = distance
+
+
+    def __repr__(self):
+        return f"{self.name} ({self.price}) - {self.rating} stars - {self.address} - {'Closed' if self.is_closed else 'Open'} - {self.distance} meters away"
+
+    def __eq__(self, other):
+        if isinstance(other, Restaurant):
+            return self.name == other.name
+        return False
+
+    def __hash__(self):
+        return hash(self.name)
+
+
 api_url = 'https://api.yelp.com/v3/businesses/search'
 api_key = 'TEDWqCrmKb_w37qkJjS426xfoUGpBF1EQBdjxcJtKeXHgJEvAmCW4zFKrHFDvuUGN_ELrNQgEOzaK9taFY-DwvpjiPx1d7xtM5cZ3vnXA2gfEeCgAsAa32_cZY1rZnYx'
 
@@ -69,10 +93,10 @@ else:
 
 # need to make sure this list is sorted by value in descending order 
 temp_list = {
-    'mexican': 4,
-    'tacos': 3,
-    'latin': 2,
-    'indian': 1,
+    'mexican': 3,
+    'tacos': 2,
+    'latin': 1,
+    'indian': 0,
     'vietnamese': 0,
     'chinese': 0, 
     'sandwich': 0, 
@@ -94,16 +118,16 @@ temp_list = {
     'dim sum': 0,
     'byo': 0, #translate to many separate yelp lookups: byo bowl, salad, chipotle, sandwich, sub, mod
     'fries': 0,
-    'rice': 0
+    'rice': 3
 } 
 
 restaurant_options = {}
-secondary_options = {}
 
 needDelivery = False 
 priceRange = ['$','$$']
 maxPrice = priceRange[-1]
 
+# !!!! I THINK THE WAY WE ARE DOING IT HERE IS NOT AS SPECIFIC TO CUISINE - need to really drastically change the value based on the cuisine here 
 #first population of the possible list(the broadest version, we will narrow down from here after this)
 for key, value in temp_list.items():
     if (value != 0):
@@ -117,40 +141,60 @@ for key, value in temp_list.items():
         if response.status_code == 200:
             data = response.json()
             for business in data.get('businesses', []):
-                if business['name'] in restaurant_options:
+                restaurant = Restaurant(
+                    name=business['name'],
+                    address=business['location']['address1'],
+                    price=str(business.get('price', 'N/A')),
+                    rating=business.get('rating', 0),
+                    latitude=business['coordinates']['latitude'],
+                    longitude=business['coordinates']['longitude'],
+                    is_closed=business.get('is_closed', True),
+                    distance=business.get('distance', 0)
+                )
+                if restaurant in restaurant_options:
                     # Add the value to the existing value
-                    restaurant_options[business['name']] += value
+                    restaurant_options[restaurant] *= value
                 else:
                     # Add the restaurant to the dictionary with the value
-                    restaurant_options[business['name']] = value
+                    restaurant_options[restaurant] = value
         else:
             print(response.status_code)
 
-print(restaurant_options)
+
+print(restaurant_options) # idk what format this is going to print in 
 
 # next: go through restaurant_options and change the score for each restaurant depending on the other factors: 
     # Factors: price range, distance (need a separate function to calculate this), ratings/stars 
 
-final_restaurant_list = {}
+# final_restaurant_list = {}
+# secondary_options = {}
 
-for key, value in restaurant_options.items():
-    params['term'] = key
-    response = requests.get(api_url, headers=HEADERS, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        for business in data.get('businesses', []):
-            if business['price'] in priceRange:
-                if business['rating'] >= 3.5: 
-                    final_restaurant_list[key] = value
-                else:
-                    if business['rating'] >= 3:
-                        secondary_options[key] = value -1
-            else:
-                if maxPrice+'$' == business['price']:  # this is how i'm trying to say it's ONE more $ sign, idk if it's right 
-                    secondary_options[key] = value
+# for key, value in restaurant_options.items():
+#     if restaurant.price in priceRange:
+#         print('restaurant is in price range.')
+#         print('checking {restaurant.name} with rating: {restaurant.rating}')
+#         if restaurant.rating >= 3.5: 
+#             print('restaurant has a great rating')
+#             final_restaurant_list[key] = value
+#         else:
+#             print('restaurant has below 3.5 stars')
+#             if restaurant.rating >= 3:
+#                 secondary_options[key] = value -1
+#     else:
+#         print('restaurant is not in price range.')
+#         if maxPrice+'$' == restaurant.price:  # this is how i'm trying to say it's ONE more $ sign, idk if it's right 
+#             secondary_options[key] = value
 
-print(final_restaurant_list)
-print(secondary_options)
+# # print(final_restaurant_list)
+# # print(secondary_options) 
+
+# print('Final Restaurant List: ')
+# for key, value in final_restaurant_list.items():
+#     print(key.name, ': ', value)
+
+# print('Secondary Restaurant List: ')
+# for key, value in secondary_options.items():
+#     print(key.name, ': ', value)
 
 
     
